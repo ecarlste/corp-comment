3a2e6bc8c8ddb426a78642a980aa116ed600b7e8
Erik S. Carlsten <ecarlste@gmail.com>
2025-04-06 13:07:33 -0600

    finished styling the feedback form in the page heading

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,5 +1,22 @@
 function FeedbackForm() {
-  return <form>FeedbackForm</form>;
+  return (
+    <form className="form">
+      <textarea
+        id="feedback-textarea"
+        placeholder="generic"
+        spellCheck={false}
+      />
+      <label htmlFor="feedback-textarea">
+        Enter your feedback here, remeber to #hashtag the company
+      </label>
+      <div>
+        <p className="u-italic">150</p>
+        <button>
+          <span>Submit</span>
+        </button>
+      </div>
+    </form>
+  );
 }
 
 export default FeedbackForm;
