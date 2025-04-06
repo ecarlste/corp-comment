5a50d8c194d87d0b01824f4f6a73830f0bf2a691
Erik S. Carlsten <ecarlste@gmail.com>
2025-04-06 14:55:15 -0600

    fetch live data from bytegrad data source for feedback list items

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,30 +1,25 @@
-import { v4 as uuidv4 } from "uuid";
 import { FeedbackItem } from "../lib/types";
 import FeedbackListItem from "./FeedbackListItem";
-
-const feedbackItemList: FeedbackItem[] = [
-  {
-    id: uuidv4(),
-    upvoteCount: 423,
-    badgeLetter: "G",
-    companyName: "Google",
-    text: "The #Google guys are amazing! I love their service and the support is top-notch.",
-    daysAgo: 4,
-  },
-  {
-    id: uuidv4(),
-    upvoteCount: 596,
-    badgeLetter: "S",
-    companyName: "Starbucks",
-    text: "I had a great experience with #Starbucks. Their coffee is fantastic and the staff are friendly.",
-    daysAgo: 3,
-  },
-];
+import { useEffect, useState } from "react";
 
 function FeedbackList() {
+  const [feedbackItems, setFeedbackItems] = useState<FeedbackItem[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "http://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
+      );
+      const data = await response.json();
+      setFeedbackItems(data.feedbacks);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <ol className="feedback-list">
-      {feedbackItemList.map((feedbackItem) => (
+      {feedbackItems.map((feedbackItem) => (
         <FeedbackListItem key={feedbackItem.id} feedbackItem={feedbackItem} />
       ))}
     </ol>
