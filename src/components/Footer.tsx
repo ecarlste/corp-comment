31c37142e688cee45068037a595e50f6416b6c74
Erik S. Carlsten <ecarlste@gmail.com>
2025-04-06 11:43:02 -0600

    initial stylesheet created with small adjustments to layout classNames

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,13 @@
 function Footer() {
   return (
-    <footer>
+    <footer className="footer">
       <small>
         <p>
           &copy; Copyright by <a href="">TheCodeCartel</a>.
+        </p>
+        <p>
+          Crafted <span className="u-bold u-italic">with love</span> so you can
+          give the big boys feedback.
         </p>
       </small>
     </footer>
