9278a4ab49ab1cb140f233f5fe6033bbc0c5888e
Erik S. Carlsten <ecarlste@gmail.com>
2025-04-06 14:58:11 -0600

    show spinner in place of feedback list while data is still loading

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,17 +1,26 @@
 import { FeedbackItem } from "../lib/types";
 import FeedbackListItem from "./FeedbackListItem";
 import { useEffect, useState } from "react";
+import Spinner from "./Spinner";
 
 function FeedbackList() {
   const [feedbackItems, setFeedbackItems] = useState<FeedbackItem[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch(
-        "http://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
-      );
-      const data = await response.json();
-      setFeedbackItems(data.feedbacks);
+      setIsLoading(true);
+
+      try {
+        const response = await fetch(
+          "http://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
+        );
+        const data = await response.json();
+
+        setFeedbackItems(data.feedbacks);
+      } finally {
+        setIsLoading(false);
+      }
     };
 
     fetchData();
@@ -19,6 +28,7 @@ function FeedbackList() {
 
   return (
     <ol className="feedback-list">
+      {isLoading && <Spinner />}
       {feedbackItems.map((feedbackItem) => (
         <FeedbackListItem key={feedbackItem.id} feedbackItem={feedbackItem} />
       ))}
