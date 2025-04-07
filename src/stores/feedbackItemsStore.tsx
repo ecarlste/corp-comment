236e3613929583fbe30ea537d028c88f156d0236
Erik S. Carlsten <ecarlste@gmail.com>
2025-04-06 19:03:49 -0600

    added zustand app state store in feedbackItemsStore

---
+++ src/stores/feedbackItemsStore.tsx
@@ -0,0 +1,105 @@
+import { create } from "zustand";
+import { FeedbackItem } from "../lib/types";
+
+type FeedbackItemsStoreState = {
+  feedbackItems: FeedbackItem[];
+  isLoading: boolean;
+  errorMessage: string;
+  selectedCompany: string;
+};
+
+type FeedbackItemsStoreActions = {
+  getCompanyList: () => string[];
+  getFilteredFeedbackItems: () => FeedbackItem[];
+  addItemToList: (text: string) => void;
+  selectCompany: (company: string) => void;
+  fetchFeedbackItems: () => void;
+};
+
+type FeedbackItemsStore = FeedbackItemsStoreState & FeedbackItemsStoreActions;
+
+export const useFeedbackItemsStore = create<FeedbackItemsStore>()(
+  (set, get) => ({
+    feedbackItems: [],
+    isLoading: false,
+    errorMessage: "",
+    selectedCompany: "",
+
+    getCompanyList: () => {
+      return [...new Set(get().feedbackItems.map((item) => item.company))];
+    },
+
+    getFilteredFeedbackItems: () => {
+      const { feedbackItems, selectedCompany } = get();
+
+      return selectedCompany === ""
+        ? feedbackItems
+        : feedbackItems.filter((item) => item.company === selectedCompany);
+    },
+
+    addItemToList: async (text: string) => {
+      const company = text
+        .split(" ")
+        .find((word) => word.startsWith("#"))
+        ?.substring(1);
+
+      const newFeedbackItem: FeedbackItem = {
+        id: new Date().getTime().toString(),
+        upvoteCount: 0,
+        badgeLetter: company?.substring(0, 1).toUpperCase() || "",
+        company: company || "Unknown",
+        text,
+        daysAgo: 0,
+      };
+
+      set((state) => ({
+        feedbackItems: [...state.feedbackItems, newFeedbackItem],
+      }));
+
+      await fetch(
+        "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks",
+        {
+          method: "POST",
+          body: JSON.stringify(newFeedbackItem),
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    },
+
+    selectCompany: (company: string) => {
+      set(() => ({
+        selectedCompany: company,
+      }));
+    },
+
+    fetchFeedbackItems: async () => {
+      set(() => ({ isLoading: true }));
+
+      try {
+        const response = await fetch(
+          "http://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
+        );
+
+        if (!response.ok) {
+          throw new Error();
+        }
+
+        const data = await response.json();
+
+        set(() => ({
+          feedbackItems: data.feedbacks,
+          errorMessage: "",
+        }));
+      } catch {
+        set(() => ({
+          errorMessage: "Something went wrong.",
+        }));
+      } finally {
+        set(() => ({ isLoading: false }));
+      }
+    },
+  })
+);
